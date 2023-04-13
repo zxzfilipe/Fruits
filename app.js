f49239a3dcdbb02091cd93f04b3297c792c93bd5
zxzfilipe <58040988+zxzfilipe@users.noreply.github.com>
2023-04-13 23:03:29 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    // Connect wallet button
+    $("#connect-button").click(function () {
+        // Add wallet connection logic here
+        console.log("Connect wallet button clicked");
+    });
+
+    // Load NFTs for sale
+    function loadNFTs() {
+        // Add logic to load NFTs from the blockchain
+        console.log("Load NFTs from the blockchain");
+    }
+
+    // Load user's NFTs
+    function loadUserNFTs() {
+        // Add logic to load user's NFTs from the blockchain
+        console.log("Load user's NFTs from the blockchain");
+    }
+
+    // Call the functions
+    loadNFTs();
+    loadUserNFTs();
+});
